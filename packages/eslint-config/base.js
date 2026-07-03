24fd088866e3d7e1069a76a18a9c63347c651024
Yul Lee Kim <yulleeki@gmail.com>
2026-07-03 22:21:08 +0900

    chore: add style rules to shared eslint config

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -3,6 +3,8 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -19,6 +21,29 @@ export const config = [
     },
     rules: {
       "turbo/no-undeclared-env-vars": "warn",
+    },
+  },
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": "warn",
+
+      "no-console": "warn",
+      "no-debugger": "error",
+      "no-unused-vars": "off",
+
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports" },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
   {
